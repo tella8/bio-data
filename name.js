1d16916ee3d49ed3bc09cd55c28fbc4e7d566f85
tella8 <maleektella9@gmail.com>
2022-06-24 16:02:11 +0100

    https://github.com/tella8/bio

---
+++ name.js
@@ -0,0 +1,15 @@
+var firstName = 'Abdul Malik';
+var middleName = 'Imam';
+var lastName = 'Tella';
+var height = '1.71m';
+var country = 'Nigeria';
+let bi0;
+bio = lastName + ' ' + middleName + ' ' + firstName;
+document.write('Name: ' + '' + bio + '<br/>');
+document.write('height: ' + height + '<br/>') ;
+document.write('country: ' + ' ' + country);
+console.log(bio);
+console.log(height);
+console.log(country);
+
+
